ad32bcd921f2c1c8f13124a1b93929482a496890
MrVolkerball <43345503+jjgarciajz@users.noreply.github.com>
2018-11-28 13:38:51 +0100

    Ready to launch Implemented uuid instead of device_id Implemented API token in tunnel confirmation
    ToDo
    MQTT with SSL
    
    Signed-off-by: Jose Garcia <43345503+jjgarciajz@users.noreply.github.com>

--- tunnel-agent.py
+++ tunnel-agent.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
- Tunnel Agent v1.1 for Machinon2
+ Agent Machinon v1.1 for Machinon2
 
  (c) Logic Energy 2018
  MGC 2018-07-27
@@ -20,25 +20,16 @@
 
 """
 
-# Loading the .env configuration variables
-from dotenv import load_dotenv
-load_dotenv()
 import os
-
-import sys                  # for argv
-import argparse             # easy argv parsing
+import sys  # for argv
+import argparse  # easy argv parsing
 import socket
 import paho.mqtt.client as pahomqtt
 import time
-# import datetime
-# import logging
 import logging.handlers
-import atexit                           # for pre-exit cleanup
-#import urllib.parse
-import urllib.request     # for HTTP functions (GET/POST request and response handling)
+import atexit  # for pre-exit cleanup
+import urllib.request  # for HTTP functions (GET/POST request and response handling)
 import urllib.error
-# import threading
-# import random
 import fcntl
 import struct
 import binascii
@@ -46,14 +37,16 @@ import json
 import subprocess
 import re
 
-# SSL Certs definitions
-CA_CERT_PATH = ""
+# Loading the .env configuration variables
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # MQTT Broker definitions
 MQTT_SERVER_HOST = os.getenv('MQTT_SERVER_HOST')
-MQTT_SERVER_PORT = os.getenv('MQTT_SERVER_PORT')
-MQTT_SERVER_PORT_SSL = os.getenv('MQTT_SERVER_PORT_SSL')
-MQTT_SERVER_USE_SSL = os.getenv('MQTT_SERVER_USE_SSL')
+MQTT_SERVER_PORT = int(os.getenv('MQTT_SERVER_PORT'))
+MQTT_SERVER_PORT_SSL = int(os.getenv('MQTT_SERVER_PORT_SSL'))
+MQTT_SERVER_USE_SSL = bool(os.getenv('MQTT_SERVER_USE_SSL'))
 MQTT_SERVER_USERNAME = os.getenv('MQTT_SERVER_USERNAME')
 MQTT_SERVER_PASSWORD = os.getenv('MQTT_SERVER_PASSWORD')
 MQTT_CERTS_PATH = os.getenv('MQTT_CERTS_PATH')
@@ -64,13 +57,16 @@ MQTT_TOPIC_PREFIX_REMOTECONNECT = os.getenv('MQTT_TOPIC_PREFIX_REMOTECONNECT')
 
 # Misc general definitions
 PROG_NAME = "Machinon Tunnel Agent"
-PROG_VERSION = "1.1.0"    # program version as string
+PROG_VERSION = "1.1.0"  # program version as string
 
-LOG_FILE = os.getenv('LOG_FILE')   # script user must have write access to this file or folder
+LOG_FILE = os.getenv('LOG_FILE')  # script user must have write access to this file or folder
 LOG_FILE_MAX_SIZE = 10 * 1024 * 1024
 
 SSH_KEY_FILE = os.getenv('SSH_KEY_FILE')
 SSH_USERNAME = os.getenv('SSH_USERNAME')
+
+# SSL Certs definitions
+CA_CERT_PATH = os.getenv('CA_CERT_PATH')
 
 # Remote ports the autossh will connect to on demand
 MIN_REMOTE_PORT = 10000
@@ -78,11 +74,13 @@ MAX_REMOTE_PORT = 65535
 
 # Port where the local nginx/apache serves the remachinon_web app
 DEFAULT_LOCAL_PORT = 80
+
 # Program allows to run with a custom port between 80 and 65535
 MIN_LOCAL_PORT = 80
 MAX_LOCAL_PORT = 65535
 
 TUNNEL_CONFIRM_URL_BASE = os.getenv('TUNNEL_CONFIRM_URL_BASE')
+
 
 # Validates uuid received in the JSON
 def valid_uuid(uuid):
@@ -90,19 +88,20 @@ def valid_uuid(uuid):
     match = regex.match(uuid)
     return bool(match)
 
+
 # Get MAC address for specified network interface
 # Python3 version from https://stackoverflow.com/questions/28927958/python-get-mac-address/34922412#34922412
 def get_mac_address(ifname):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    info = fcntl.ioctl(s.fileno(), 0x8927,  struct.pack('256s',  bytes(ifname[:15], 'utf-8')))
+    info = fcntl.ioctl(s.fileno(), 0x8927, struct.pack('256s', bytes(ifname[:15], 'utf-8')))
     # Classic colon-delimited MAC
-    #return ''.join(l + ':' * (n % 2 == 1) for n, l in enumerate(binascii.hexlify(info[18:24]).decode('utf-8')))[:-1]
+    # return ''.join(l + ':' * (n % 2 == 1) for n, l in enumerate(binascii.hexlify(info[18:24]).decode('utf-8')))[:-1]
     # 12-digit hex only
-    return ''.join(binascii.hexlify(info[18:24]).decode('utf-8'))
+    return ''.join(binascii.hexlify(info[18:24]).decode('utf-8')).upper()
 
 
 def on_connect(client, userdata, flags, result):
-    #logger.info("MQTT Connection result: " + pahomqtt.connack_string(result))
+    # logger.info("MQTT Connection result: " + pahomqtt.connack_string(result))
     if result == 0:
         # got connected OK
         logger.info("MQTT Connected OK.")
@@ -117,6 +116,7 @@ def on_message(client, userdata, message):
     global tunnel_do_close
     global remote_port_num
     global tunnel_uuid
+    global tunnel_token
 
     message_string = message.payload.decode("utf-8")
     logger.debug("MQTT_msg: Topic='" + message.topic + "'   Payload='" + message_string + "'")
@@ -126,42 +126,51 @@ def on_message(client, userdata, message):
         return
 
     try:
-        messasge_dict = json.loads(message_string)
+        message_dict = json.loads(message_string)
     except json.JSONDecodeError as exc:
         logger.debug("MQTT JSON decode error: " + str(exc))
     else:
-        logger.debug("MQTT JSON decoded: " + str(messasge_dict))
-        if "tunnel" in messasge_dict and messasge_dict["tunnel"] == "open":
-            if "port" in messasge_dict:
+        logger.debug("MQTT JSON decoded: " + str(message_dict))
+        if "tunnel" in message_dict and message_dict["tunnel"] == "open":
+            if "port" in message_dict:
                 remote_port_num = 0
                 try:
-                    remote_port_num = int(messasge_dict["port"])
+                    remote_port_num = int(message_dict["port"])
                 except:
                     logger.info("MQTT JSON: Bad port value")
                 else:
                     if remote_port_num >= MIN_REMOTE_PORT and remote_port_num <= MAX_REMOTE_PORT:
                         tunnel_uuid = ""
-                        if "tunnel_uuid" in messasge_dict:
+                        if "uuid" in message_dict:
                             try:
-                                tunnel_uuid = str(messasge_dict["tunnel_uuid"])
+                                tunnel_uuid = str(message_dict["uuid"])
                             except:
                                 logger.info("MQTT JSON: Bad tunnel_uuid value")
                             else:
                                 if valid_uuid(tunnel_uuid):
-                                    logger.info("MQTT got tunnel open command with port=" + str(remote_port_num) + " tunnel_uuid=" + str(tunnel_uuid))
+                                    logger.info("MQTT got tunnel open command with port=" + str(
+                                        remote_port_num) + " tunnel_uuid=" + str(tunnel_uuid))
                                     # trigger r-ssh open
                                     tunnel_do_open = True
+                                    tunnel_token = ""
+                                    if "access_token" in message_dict:
+                                        try:
+                                            tunnel_token = str(message_dict["access_token"])
+                                        except:
+                                            logger.info("MQTT JSON: Bad access_token value")
+                                        else:
+                                            logger.info("MQTT JSON: All okay!")
                                 else:
                                     logger.info("MQTT JSON: Bad tunnel_uuid value")
                         else:
                             logger.info("MQTT JSON: tunnel_uuid not specified. Confirmation will be skipped.")
                             # trigger r-ssh open
-                            tunnel_do_open = True
+                            tunnel_do_open = False
                     else:
                         logger.info("MQTT JSON: Bad port value")
             else:
                 logger.debug("MQTT JSON: port number not specified!")
-        elif "tunnel" in messasge_dict and messasge_dict["tunnel"] == "close":
+        elif "tunnel" in message_dict and message_dict["tunnel"] == "close":
             # terminate r-ssh session
             logger.info("MQTT got tunnel close command")
             tunnel_do_close = True
@@ -179,7 +188,8 @@ def on_disconnect(client, userdata, result):
 
 
 def on_subscribe(client, userdata, mid, granted_qos):
-    logger.info("MQTT Subscribed to '" + MQTT_TOPIC_PREFIX_REMOTECONNECT + "/" + mac_address + "' QOS=" + str(granted_qos))
+    logger.info(
+        "MQTT Subscribed to '" + MQTT_TOPIC_PREFIX_REMOTECONNECT + "/" + mac_address + "' QOS=" + str(granted_qos))
 
 
 def cleanup():
@@ -195,7 +205,7 @@ def cleanup():
         if not ssh_output:
             logger.info("Tunnel closed")
     except subprocess.CalledProcessError as e:
-        #logger.info("Tunnel close failed or no existing tunnel: " + str(e))
+        # logger.info("Tunnel close failed or no existing tunnel: " + str(e))
         logger.info("Tunnel close failed or no existing tunnel")
 
 
@@ -208,19 +218,20 @@ def main(argv):
     global tunnel_do_close
     global remote_port_num
     global tunnel_uuid
+    global tunnel_token
     global local_port_num
-    
-    #ssh_process = None
-    #ssh_output = ""
+
+    # ssh_process = None
+    # ssh_output = ""
 
     atexit.register(cleanup)
 
-    mac_address = get_mac_address('eth0')
+    mac_address = 'B827EB8B4A89'  # get_mac_address('eth0')
     client_id = MQTT_CLIENT_ID_PREFIX + mac_address
     logger.info(PROG_NAME + " version " + PROG_VERSION + " started. MQTT ClientID = " + client_id)
     logger.info("Machinon MAC: " + mac_address)
     logger.info("Local server port: " + str(local_port_num))
-    #sys.exit(0)
+    # sys.exit(0)
 
     paho_client = pahomqtt.Client(client_id, True, None, pahomqtt.MQTTv311, "tcp")
     paho_client.on_connect = on_connect
@@ -232,7 +243,7 @@ def main(argv):
     # paho_client.max_inflight_messages_set(50)
     # TODO set up SSL if required
     if MQTT_SERVER_USE_SSL:
-        paho_client.tls_set()    # use default system certs path/files
+        paho_client.tls_set()  # use default system certs path/files
 
     try:
         if MQTT_SERVER_USE_SSL:
@@ -248,7 +259,7 @@ def main(argv):
 
     while True:
         # loop forever, waiting for MQTT messages and starting/stopping tunnel as required
-        time.sleep(0.2)   # is there a better way to yield control to system?
+        time.sleep(0.2)  # is there a better way to yield control to system?
 
         # check to see if tunnel  open or close has been requested
         if tunnel_do_open:
@@ -267,7 +278,7 @@ def main(argv):
                     if not ssh_output:
                         logger.info("Existing tunnel closed")
             except subprocess.CalledProcessError as e:
-                #logger.info("Tunnel close failed or no existing tunnel: " + str(e))
+                # logger.info("Tunnel close failed or no existing tunnel: " + str(e))
                 logger.info("Tunnel close failed or no existing tunnel")
 
             # try to open a new tunnel
@@ -279,7 +290,7 @@ def main(argv):
                 #   "-R <remote_port>:localhost:<local_port>" = open a tunnel between local_port and remote_port
                 #   "-N" = do not execute any remote command (only use the connection for forwarding)
                 full_ssh_command = "sudo autossh -f -N -i %s -R %d:localhost:%d %s" % (
-                SSH_KEY_FILE, remote_port_num, local_port_num, SSH_USERNAME)
+                    SSH_KEY_FILE, remote_port_num, local_port_num, SSH_USERNAME)
                 ssh_output = subprocess.check_output(full_ssh_command, shell=True)
                 if not ssh_output:
                     logger.info("Tunnel opened")
@@ -287,12 +298,19 @@ def main(argv):
                 logger.info("Tunnel open failed: " + str(e))
             else:
                 # tunnel opened, so call LE API to confirm that we are online
-                if valid_uuid(tunnel_uuid):
+                if valid_uuid(tunnel_uuid) and tunnel_token:
+                    headers = {
+                        'Authorization': 'Bearer ' + tunnel_token,
+                        'Content-Type': 'application/json',
+                        'X-Requested-With': 'XMLHttpRequest',
+                    }
                     # This line calls the new Remachinon App (Laravel) confirmation endpoint
-                    req = urllib.request.Request(TUNNEL_CONFIRM_URL_BASE + '/tunnels/' + str(tunnel_uuid) + '/confirm')
+                    confirm_url = TUNNEL_CONFIRM_URL_BASE + '/tunnels/' + str(tunnel_uuid) + '/confirm'
+                    logger.debug('Calling : ' + confirm_url)
+                    req = urllib.request.Request(confirm_url, None, headers, method='PUT')
                     try:
                         response = urllib.request.urlopen(req)
-                        #response_page = response.read()
+                        # response_page = response.read()
                     except urllib.error.URLError as e:
                         logger.info("Confirmation API GET failed: " + str(e))
                     else:
@@ -304,7 +322,7 @@ def main(argv):
             # try to close the tunnel
             tunnel_do_close = False
             logger.info("Attempting to close tunnel...")
-            #ssh_process.terminate()
+            # ssh_process.terminate()
             try:
                 # TODO use more secure/graceful way to stop autossh
                 ssh_output = subprocess.check_output("sudo killall autossh", shell=True)
@@ -312,7 +330,7 @@ def main(argv):
                     logger.info("Tunnel closed")
             except subprocess.CalledProcessError as e:
                 logger.info("Tunnel close failed or no existing tunnel")
-                #logger.info("Tunnel close failed or no existing tunnel: " + str(e))
+                # logger.info("Tunnel close failed or no existing tunnel: " + str(e))
 
 
 rootlogger = logging.getLogger()
@@ -329,11 +347,11 @@ logfilehandler.setLevel(logging.DEBUG)
 rootlogger.addHandler(logfilehandler)
 
 # Add a Linux syslog handler
-#sysloghandler = logging.handlers.SysLogHandler(
+# sysloghandler = logging.handlers.SysLogHandler(
 
 # define a Handler which writes INFO messages or higher to the sys.stderr
 console = logging.StreamHandler()
-#console.setLevel(logging.DEBUG)
+# console.setLevel(logging.DEBUG)
 console.setFormatter(formatter)
 rootlogger.addHandler(console)
 
@@ -353,8 +371,10 @@ tunnel_uuid = ""
 paho_client = None
 
 parser = argparse.ArgumentParser(description=PROG_NAME + " " + PROG_VERSION)
-parser.add_argument('-p', '--port', help='Local port to forward to, e.g. "-p 80" for local httpd (default is 80)', required=False, type=int)
-parser.add_argument('-v', '--version', help='Print version info', action='version', version=PROG_NAME + " version " + PROG_VERSION)
+parser.add_argument('-p', '--port', help='Local port to forward to, e.g. "-p 80" for local httpd (default is 80)',
+                    required=False, type=int)
+parser.add_argument('-v', '--version', help='Print version info', action='version',
+                    version=PROG_NAME + " version " + PROG_VERSION)
 args = parser.parse_args()
 if args.port:
     if MIN_LOCAL_PORT <= args.port <= MAX_LOCAL_PORT:
